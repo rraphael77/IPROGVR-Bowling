5f6433fbddb1d30251670915691386a5c33123e7
rraphael77 <rraphael77teo@gmail.com>
2025-08-14 23:44:41 +0800

    Update 1

---
+++ Assets/Scripts/BallTriggerL.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public class BallTriggerL : MonoBehaviour
+{
+    public Animator BallBlockerL;
+
+    private bool ballTrigger = false;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("BowlingBall"))
+        {
+            ballTrigger = true;
+
+            Debug.Log("Bowling Ball exited the trigger!");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LaneStart());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ballTrigger)
+        {
+            ballTrigger = false;
+
+            StartCoroutine(LaneReset());
+        }
+    }
+
+    IEnumerator LaneStart()
+    {
+        yield return new WaitForSeconds(1f);
+
+        PinsTriggerL.Active = true;
+    }
+
+    private IEnumerator LaneReset()
+    {
+        yield return new WaitForSeconds(5);
+
+        BallBlockerL.SetTrigger("Trigger");
+    }
+}
---
+++ Assets/Scripts/PinsTriggerL.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinsTriggerL : MonoBehaviour
+{
+    public static bool Active = false;
+
+    public static int PinsKnockedL = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (Active)
+        {
+            PinsKnockedL--;
+        }
+
+        Debug.Log($"Score: {PinsKnockedL} Pins");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (Active)
+        {
+            PinsKnockedL++;
+        }
+
+        Debug.Log($"Score: {PinsKnockedL} Pins");
+    }
+}
